fc3142848166a6412cf2d741b1a36f8b9a7cb5c5
Yaroslava Pletsys <103317047+Edouena8@users.noreply.github.com>
2023-07-31 11:29:25 +0200

    added toolkit

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,11 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// export const contactsSlice = createSlice({
+//     name: "contacts",
+//     initialState: [],
+//     reducers: {
+//         addContact(state, action) {
+            
+//         }
+//     }
+// })
